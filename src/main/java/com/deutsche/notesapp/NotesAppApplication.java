5c64e059e43722b1f9d199805c003c6993fc309d
kirkondrash <kirkondrash@yandex.ru>
2020-09-15 19:59:34 +0300

    alternative load of jpa beans of both boot app and starter

--- src/main/java/com/deutsche/notesapp/NotesAppApplication.java
+++ src/main/java/com/deutsche/notesapp/NotesAppApplication.java
@@ -2,8 +2,12 @@ package com.deutsche.notesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories
+//@EntityScan
 public class NotesAppApplication {
 
     public static void main(String[] args) {
